806eefdee4688e14cdc583dd0d72934cd4c2102c
Bagaeva.A.V <a.v.bagaeva@yandex.ru>
2021-10-20 23:36:34 +0300

    lessonTwo

--- 2|_ФЛ/main.swift
+++ 2|_ФЛ/main.swift
@@ -7,5 +7,39 @@
 
 import Foundation
 
-print("Hello, World!")
+//1. Написать функцию, которая определяет, четное число или нет
+let x: Int = 5
+if x % 2 == 0 {
+    print("\(x) is even number")
+}
+else {
+    print("\(x) is odd number")
+}
+ print("____________________________")
+//2. Написать функцию, которая определяет, делится ли число без остатка на 3.
+let x1: Int = 35
+if x1 % 3 == 0 {
+    print("\(x1) is diveded in 3 without remains")
+}
+else {
+    print("\(x1) is divided in 3 with remains")
+}
+
+print("____________________________")
+
+//3. Создать возрастающий массив из 100 чисел.
+var increasingArray = Array (1...100)
+    print("\(increasingArray)")
+
+print("____________________________")
+
+//4. Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
+for value in increasingArray {
+    if value % 3 == 0 || value % 2 == 0{
+        increasingArray.remove(at: increasingArray.firstIndex(of: value)!)
+    }
+}
+print("\(increasingArray)")
+
+print("____________________________")
 
